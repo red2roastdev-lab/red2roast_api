7cfedcb3d7cef4c7614ea35844092faf90a1bb30
Trevor-Duane <trevorkayiira@gmail.com>
2025-10-25 10:12:36 +0300

    Fixing Cors

--- server.js
+++ server.js
@@ -35,6 +35,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://red2roast.shop');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
+
+
 app.use("/api", routes);
 
 //Error handling middleware
