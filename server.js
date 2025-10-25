16d87b3f22360735987a60fd788871bb6e71a04d
Trevor-Duane <trevorkayiira@gmail.com>
2025-10-25 10:46:26 +0300

    Fixing Cors

--- server.js
+++ server.js
@@ -23,26 +23,16 @@ const connectDB = async () => {
 
 connectDB();
 
-// Enable CORS for all routes
-app.use(cors({
-    origin: ['https://red2roast.shop', 'http://localhost:5173' , 'https://www.red2roast.shop'],
-    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS', 'PUT'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-  }));
-
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://red2roast.shop');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
-});
-
+// Enable CORS for all routes
+app.use(cors({
+    origin: ['https://red2roast.shop', 'http://localhost:5173', 'https://www.red2roast.shop'],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS', 'PUT'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use("/api", routes);
 
