cd2e464101e7a7163d048a6d67b1349d370869d0
Trevor-Duane <trevorkayiira@gmail.com>
2025-10-25 09:50:07 +0300

    Fixing Cors

--- server.js
+++ server.js
@@ -23,19 +23,17 @@ const connectDB = async () => {
 
 connectDB();
 
+// Enable CORS for all routes
+app.use(cors({
+    origin: ['https://red2roast.shop', 'http://localhost:5173' , 'https://www.red2roast.shop'],
+    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS', 'PUT'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  }));
 
-app.use(cors());
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.options('*', (req, res) => {
-//   res.header('Access-Control-Allow-Origin', 'https://red2roast.shop');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.sendStatus(200);
-// });
 
 app.use("/api", routes);
 
@@ -49,23 +47,3 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Red2Roast Server listening on port ${PORT}!`))
 
-// const PORT = process.env.PORT
-// const HOST = process.env.HOST
-
-// async function startServer() {
-//     try {
-//         await sequelize.authenticate();
-//         console.log("Oooh yeah, Database connected");
-
-//         await sequelize.sync({ alter: true });
-//         console.log("And all models synced");
-
-//         app.listen(PORT, () => {
-//             console.log(`Server running at http://${HOST}:${PORT}`);
-//         });
-//     } catch (error) {
-//         console.error("Oops, DB connection failed:", error);
-//     }
-// }
-
-// startServer();
