4ea0753508c3849682e1c8c3bb6be53f987788f0
Trevor-Duane <trevorkayiira@gmail.com>
2025-11-10 14:47:09 +0300

    Live Host Fix

--- services/nodemailerWelcomeEmail.js
+++ services/nodemailerWelcomeEmail.js
@@ -10,7 +10,7 @@ export const nodemailerWelcomeEmail = async (lead) => {
         const token = generateActivationToken({email})
 
         //Dynamic links
-        const activationLink = `${process.env.LOCAL_HOST}/additional_information?r=${token}`;
+        const activationLink = `${process.env.LIVE_HOST}/additional_information?r=${token}`;
 
         const mailOptions = {
             from: `"Red2Roast Team" <${process.env.MC_EMAIL_SENDER}>`,
