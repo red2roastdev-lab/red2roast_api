4880f9867e951ff2cfae2a95ba4c4b8c74f13184
Trevor-Duane <trevorkayiira@gmail.com>
2025-11-15 01:54:51 +0300

    Email User

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors({
 }));
 
 // Explicitly handle OPTIONS preflight requests
-app.options('/*', (req, res) => {
+app.options('/api/*', (req, res) => {
   res.header('Access-Control-Allow-Origin', allowedOrigins.includes(req.headers.origin) ? req.headers.origin : '');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
