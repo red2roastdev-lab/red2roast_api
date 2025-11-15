794b63f65ae7baa049ec27802f32c61fa6bb1b90
Trevor-Duane <trevorkayiira@gmail.com>
2025-11-15 11:20:01 +0300

    Cors

--- server.js
+++ server.js
@@ -29,28 +29,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["POST", "GET", "DELETE", "OPTIONS", "PATCH"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Accept", "Authorization"],
-  })
-);
+app.use(cors());
 
-// FIXED preflight handler
-app.options("/{*all}", (req, res) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  return res.sendStatus(204);
-});
 
 app.use("/api", routes);
 
