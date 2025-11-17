cf2e4125d4f8de25733f97c6179a5d2b2ea9412e
Trevor-Duane <trevorkayiira@gmail.com>
2025-11-17 14:08:15 +0300

    emails fix

--- controllers/leads/lead.js
+++ controllers/leads/lead.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import { customAlphabet } from 'nanoid';
 import { ShopifyWelcomeDC } from "../../shopify/ShopifyWelcome.js";
 import { ShopifyFreeDeliveryNL } from "../../shopify/ShopifyFreeDelivery.js";
+import welcomeQueue from "../../queues/welcomeQueue.cjs";
+import referralQueue from "../../queues/referralQueue.cjs";
 import db from "../../config/database.js";
 import { WelcomeEmail } from "../../services/WelcomeEmail.js";
 import { ReferralEmail } from "../../services/ReferralEmail.js";
@@ -96,19 +98,13 @@ export const createLead = async (req, res) => {
         }, { transaction: t });
       }
 
-      // Activation Email: Fire-and-forget async call
-      (async () => {
-        try {
-          await WelcomeEmail({
-            lead_name: lead.name,
-            lead_email: lead.email,
-            referral_code: lead.referral_code,
-            couponCode: leadCoupon.code,
-          });
-        } catch (err) {
-          console.error("Failed to send welcome email:", err.message);
-        }
-      })();
+      // Queue welcome email
+      await WelcomeEmail({
+        lead_name: lead.name,
+        lead_email: lead.email,
+        referral_code: lead.referral_code,
+        coupon_code: welcomeCoupon.code,
+      });
 
       return { lead, welcomeCoupon, deliveryCoupon };
     });
@@ -175,18 +171,13 @@ export const handleReferredFriend = async (req, res) => {
       status: "pending"
     });
 
-    // send referral email
-    (async () => {
-      try {
-        await ReferralEmail({
-          friend_email: friendEmail,
-          referrer_name: lead.name,
-          referral_code: referralCode
-        });
-      } catch (err) {
-        console.error("Failed to send welcome email:", err.message);
-      }
-    })();
+    // send referral email via queue
+    await ReferralEmail({
+      friend_email: friendEmail,
+      referrer_name: lead.name,
+      referral_code: referralCode
+    });
+
 
     return res.json({ message: "Referral invite sent successfully" });
 
